a2e11abc039ed8c00b11206fab37b839c79ac5d9
Chip <chipz68q@gmail.com>
2018-11-24 16:30:03 +0500

    Add print_message function
    This function print to stdout message string
    followed by newline.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+
+static void print_message(void *message);
+
 int main()
 {
+}
+
+static void print_message(void *message)
+{
+	while (1)
+		printf("%s\n", (const char *) message);
 }
