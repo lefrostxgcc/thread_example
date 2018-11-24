a8339ce8695b6f99da3647554c13523ec032e013
Chip <chipz68q@gmail.com>
2018-11-24 20:39:53 +0500

    Implement 2 threads by clone call

--- src/main.c
+++ src/main.c
@@ -1,11 +1,32 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <unistd.h>
 #include <stdio.h>
+
+enum { THREAD_COUNT = 2, STACK_SIZE = 1 << 16 };
+static char stack[THREAD_COUNT][STACK_SIZE];
 
 static int print_message(void *message);
 
 int main()
 {
-	print_message("First");
+	int		flags;
+
+	flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD;
+
+	if (clone(print_message, stack[0] + STACK_SIZE, flags, "First") < 0)
+	{
+		perror("clone");
+		return 1;
+	}
+
+	if (clone(print_message, stack[1] + STACK_SIZE, flags, "\tSECOND") < 0)
+	{
+		perror("clone");
+		return 1;
+	}
+
+	print_message("\t\tThird");
 }
 
 static int print_message(void *message)
