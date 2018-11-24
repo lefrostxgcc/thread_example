2a572b5acafe095486dc93183a2f38dc70e53749
Chip <chipz68q@gmail.com>
2018-11-24 20:32:49 +0500

    Modify return type to int
    This is need for correct use in clone(2) call

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-static void print_message(void *message);
+static int print_message(void *message);
 
 int main()
 {
 	print_message("First");
 }
 
-static void print_message(void *message)
+static int print_message(void *message)
 {
 	while (1)
 		printf("%s\n", (const char *) message);
+
+	return 0;
 }
